13e84ffe38c5732e42f1f5f1b5dccd64e8babe4d
Jon Kline <jonphilk@gmail.com>
2020-08-29 11:16:13 -0400

    Note models and service

---
+++ ElevenNote.Models/NoteCreate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteCreate
+    {
+        [Required]
+        [MinLength(1, ErrorMessage = "Enter at least 1 character.")]
+        [MaxLength(100, ErrorMessage = "Title limit is 100 characters.")]
+        public string Title { get; set; }
+
+        [MaxLength(8000, ErrorMessage = "Max length is 8000 characters.")]
+        public string Content { get; set; }
+    }
+}
