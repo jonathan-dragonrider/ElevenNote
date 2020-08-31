b0b272de348d69a1276e68536b1509667ff5863b
Jon Kline <jonphilk@gmail.com>
2020-08-31 08:39:21 -0400

    Added structure for Category

--- ElevenNote.Data/Note.cs
+++ ElevenNote.Data/Note.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,5 +27,8 @@ namespace ElevenNote.Data
         public DateTimeOffset CreatedUtc { get; set; }
 
         public DateTimeOffset? ModifiedUtc { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
     }
 }
